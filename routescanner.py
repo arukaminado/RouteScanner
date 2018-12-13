f5beb2f10d8a5b21c86b52067d79b186f756e915
Vidyasagar Kumar <vkumar03@Vidyasagars-MacBook-Pro.local>
2018-12-13 17:39:39 +0530

    New Json working

--- routescanner.py
+++ routescanner.py
@@ -1,6 +1,8 @@
 import fileopener
 import re
 import ipaddress
+import json
+import sys
 
 def valid_ip(address):
     try: 
@@ -9,17 +11,28 @@ def valid_ip(address):
     except:
         return False
 
+#routescan=fileopener.fileread('routefile.txt')
+with open('routefile.txt',encoding='utf-8') as data:
+    resp_dict=json.loads(data.read())
+    #routes learnt from the router
+    cidrblocklearnt=resp_dict['route-information'][0]['route-table'][0]['rt'][0]['rt-destination'][0]['data']
+    #nexthopdetail=resp_dict['route-information'][0]['route-table'][0]['rt'][0]['rt-destination'][0]['nh'][0]['via']
+    subnetextract=re.findall(r'/[0-9]{1,3}',cidrblocklearnt)
+    hello=re.findall(r'[0-9]{1,3}',subnetextract[0])
+    hello1=int(hello[0])
+    print(hello1)
+    IPextract=re.findall(r'[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}',cidrblocklearnt)
+    print(subnetextract)
+    print(IPextract)
+   # print (resp_dict['route-information'][0]['route-table'][0]['rt'][0]['rt-destination'][0]['data'])
+    print (resp_dict['route-information'][0]['route-table'][0]['rt'][0]['rt-entry'][0]['nh'][0]['via'][0]['data'])
 
-routescan=fileopener.fileread('routefile.txt')
-for p,q in enumerate(routescan):
-    routescan[p]=q.strip()
 
-for j in routescan:
-#    routescanclean=list(map(str.strip,j))
-    ipaddressstrip=re.findall(r'[0-9]+(?:\.)[0-9]+(?:\.)[0-9]+(?:\.)+.....',j)
-    print (ipaddressstrip)
-#for i in routescanclean:
- #   testIP= valid_ip(i)
+
     
-        
+
+
+
+
+
 

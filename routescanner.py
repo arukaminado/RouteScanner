ce2d03fe7afb1ddceb08098dc28419e4f598aaa1
Vidyasagar Kumar <vkumar03@Vidyasagars-MacBook-Pro.local>
2018-12-13 15:07:11 +0530

    Update

--- routescanner.py
+++ routescanner.py
@@ -1,6 +1,25 @@
 import fileopener
 import re
+import ipaddress
+
+def valid_ip(address):
+    try: 
+        print (ipaddress.ip_network(address))
+        return True
+    except:
+        return False
+
+
 routescan=fileopener.fileread('routefile.txt')
-for i in routescan:
-    b= re.search(r"(.+?)+(\d) +(.+?)\s{2,}(\w)*",i)
-    print (b)
+for p,q in enumerate(routescan):
+    routescan[p]=q.strip()
+
+for j in routescan:
+#    routescanclean=list(map(str.strip,j))
+    ipaddressstrip=re.findall(r'[0-9]+(?:\.)[0-9]+(?:\.)[0-9]+(?:\.)+.....',j)
+    print (ipaddressstrip)
+#for i in routescanclean:
+ #   testIP= valid_ip(i)
+    
+        
+

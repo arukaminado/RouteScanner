e33b1c8d66a8b6851883db1f8a522b7eeaf4445f
Vidyasagar Kumar <vkumar03@Vidyasagars-MacBook-Pro.local>
2018-12-20 16:18:35 +0530

    New update

--- progfunctions.py
+++ progfunctions.py
@@ -19,18 +19,21 @@ def valid_ip(address):
 
 #Return the Int Value of the Subnet
 def subnetextract(subnetvalue):
-    subnetextract=re.findall(r'/[0-9]{1,3}',subnetvalue)
-    print(subnetextract)
-    subnetlist=re.findall(r'[0-9]{1,3}',subnetextract[0])
-    subnetint=int(subnetlist[0])
-    ipextract=str(re.findall(r'[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}',subnetvalue))
-    while subnetint<25:
-        if(subnetint>8)==True and (subnetint<25)==True:
-            subnetconvert[subnetint]=str(subnetint)
-            print(ipextract+subnetconvert[subnetint])
+    subnetextract=re.findall(r'/[0-9]{1,3}',subnetvalue) #extract the subnet from the list
+    subnetlist=re.findall(r'[0-9]{1,3}',subnetextract[0]) #extract the subnet without the /
+    subnetint=int(subnetlist[0]) #convert the subnet to integer for the loop
+    ipextract=re.findall(r'[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}',subnetvalue) #extract the IP address from the list
+    ipextractstr=str(ipextract[0]) #convert the IP address into a string value
+    ipsubnets=[] #blank list to store the IP subnets range viz. 10.10.10.10/20 >> List value 10.10.10.10/21, 10.10.10.10/22, 10.10.10.10/23 & 10.10.10.10/24
+    while subnetint<=32:
+        if(subnetint>8)==True and (subnetint<=32)==True:
+            subnetstr=str(subnetint)
+            with open('ipsubnetsfile.txt','a',encoding='utf-8') as breakupsubnets:
+                breakupsubnets.write(ipextractstr+'/'+subnetstr+'\n')
+            ipsubnets.append(ipextractstr+'/'+subnetstr)
             subnetint=subnetint+1
-
-    #return(subnetint)
+    return ipsubnets
+            
 
 #Return the subnets of a CIDR
 def subnetgenerator(cidrblock):
--- routescanner.py
+++ routescanner.py
@@ -2,27 +2,26 @@ import progfunctions
 import re
 import json
 import sys
-
-#routescan=fileopener.fileread('routefile.txt')
+i=0
 with open('routefile.txt',encoding='utf-8') as data:
     resp_dict=json.loads(data.read())
-    #routes learnt from the router
-    cidrblocklearnt=resp_dict['route-information'][0]['route-table'][0]['rt'][0]['rt-destination'][0]['data']
-    #nexthopdetail=resp_dict['route-information'][0]['route-table'][0]['rt'][0]['rt-destination'][0]['nh'][0]['via']
-    progfunctions.subnetextract(cidrblocklearnt)
-   # progfunctions.subnetgenerator(ipsubnet)
+    while True:
+        if resp_dict is None:
+            break
+        else:
+            #print(resp_dict['route-information'][0]['route-table'][0]['rt'][0]['rt-destination']['data'])
+            cidrblocklearnt=resp_dict['route-information'][0]['route-table'][0]['rt'][i]['rt-destination'][0]['data']#routes learnt from the router
+            print (cidrblocklearnt)
+        #nexthopdetail=resp_dict['route-information'][0]['route-table'][0]['rt'][0]['rt-destination'][0]['nh'][0]['via']
+            ipsubnets=progfunctions.subnetextract(cidrblocklearnt)
+#            with open('ipsubnetsfile.txt','a',encoding='utf-8') as breakupsubnets:
+#                breakupsubnets.write(ipsubnets)
+            print (ipsubnets)
+            i=i+1
+            if i >= len(resp_dict['route-information'][0]['route-table'][0]['rt']):
+                break
         
-    ipextract=re.findall(r'[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}',cidrblocklearnt)
-    print(ipextract)
-   # print (resp_dict['route-information'][0]['route-table'][0]['rt'][0]['rt-destination'][0]['data'])
-    print (resp_dict['route-information'][0]['route-table'][0]['rt'][0]['rt-entry'][0]['nh'][0]['via'][0]['data'])
-
-
-
-    
-
-
-
-
-
+        # print (resp_dict['route-information'][0]['route-table'][0]['rt'][0]['rt-destination'][0]['data'])
+        # print (resp_dict['route-information'][0]['route-table'][0]['rt'][0]['rt-entry'][0]['nh'][0]['via'][0]['data'])
+    data.close()
 
